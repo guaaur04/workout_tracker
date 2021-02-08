5b1344880d18f9cc0b56ec9cb80bf00d965aa2d8
V Guardado <auricia.guardado@gmail.com>
2021-02-07 21:51:56 -0800

    placeholder_mock_data

--- public/db.js
+++ public/db.js
@@ -2,3 +2,4 @@ let db;
 // create a new db request for a "workout" database
 
 
+
---
+++ public/mock_data.js
@@ -0,0 +1,13 @@
+const excersice = [
+
+    {
+name: "Plank Position",
+type: "Hardcore",
+weight: "The Poetry Anthology book sits on your back. It weight 50lbs",
+sets: "3",
+reps: "1"
+
+    }
+]
+
+module.exports = excersice; 
