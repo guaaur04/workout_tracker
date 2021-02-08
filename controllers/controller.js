b9c24f9ed43784068cbfbe40f1fcac4deff978e8
V Guardado <auricia.guardado@gmail.com>
2021-02-07 23:17:37 -0800

    update controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,16 @@
-//I might just ignore this?
+const express = require("express");
+const router = express.Router();
+const excercies = require("../models/exercise");
+const workout  = require("../models/workout");
+
+router.get("/", (req,res) => {
+    res.render("index",(workout: workout));
+}); 
+
+
+
+const router = require("../routes/api");
+
+router.get("/", (req.res) => {
+    console.log(workout)
+}
